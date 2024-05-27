553bdf378dd9bde3559a5b1397a6c57c214ce6da
irenaldojs <irenaldojs@gmail.com>
2024-05-27 20:57:45 -0300

    ...

--- src/compoents/Partida/index.tsx
+++ src/compoents/Partida/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import { PartidaEntity } from "../../entities/partidas.entity";
 import { dataStore } from "../../store/dataStore";
+import { Block, Check, Close, Remove } from "@mui/icons-material";
 
 export default function Partida(props: PartidaEntity) {
   const clubes = dataStore((data) => data.clubes);
@@ -17,38 +18,72 @@ export default function Partida(props: PartidaEntity) {
         display={"flex"}
         justifyContent={"center"}
         alignItems={"center"}
-        minWidth={500}
+        minWidth={700}
       >
         <Box
           display={"flex"}
-          justifyContent={"right"}
-          alignItems={"end"}
+          justifyContent={"space-between"}
           width={"45%"}
           gap={1}
         >
-          <Typography variant="h6">
-            {props.clube_casa_id && clubes[props.clube_casa_id]?.nome}
-          </Typography>
-          {props.clube_casa_id && (
-            <img src={clubes[props.clube_casa_id]?.escudos?.["45x45"]} />
-          )}
+          <Box
+            display={"flex"}
+            alignItems={"end"}
+            justifyContent={"right"}
+            minWidth={100}
+          >
+            {props.aproveitamento_mandante &&
+              props.aproveitamento_mandante.map((item) => testIcon(item))}
+          </Box>
+          <Box
+            display={"flex"}
+            gap={1}
+            alignItems={"end"}
+            justifyContent={"right"}
+          >
+            <Typography variant="h6">
+              {props.clube_casa_id && clubes[props.clube_casa_id]?.nome}
+            </Typography>
+            {props.clube_casa_id && (
+              <img src={clubes[props.clube_casa_id]?.escudos?.["45x45"]} />
+            )}
+          </Box>
         </Box>
+
         <Box width={"5%"} textAlign={"center"}>
           X
         </Box>
+
         <Box
           display={"flex"}
-          gap={1}
-          justifyContent={"left"}
-          alignItems={"end"}
+          justifyContent={"space-between"}
           width={"45%"}
+          gap={1}
         >
-          {props.clube_visitante_id && (
-            <img src={clubes[props.clube_visitante_id]?.escudos?.["45x45"]} />
-          )}
-          <Typography variant="h6">
-            {props.clube_visitante_id && clubes[props.clube_visitante_id]?.nome}
-          </Typography>
+          <Box
+            display={"flex"}
+            gap={1}
+            alignItems={"end"}
+            justifyContent={"left"}
+          >
+            {props.clube_visitante_id && (
+              <img src={clubes[props.clube_visitante_id]?.escudos?.["45x45"]} />
+            )}
+            <Typography variant="h6">
+              {props.clube_visitante_id &&
+                clubes[props.clube_visitante_id]?.nome}
+            </Typography>
+          </Box>
+
+          <Box
+            display={"flex"}
+            alignItems={"end"}
+            justifyContent={"right"}
+            minWidth={100}
+          >
+            {props.aproveitamento_visitante &&
+              props.aproveitamento_visitante.map((item) => testIcon(item))}
+          </Box>
         </Box>
       </Box>
       <Box display={"flex"} gap={1} alignItems={"end"}>
@@ -70,4 +105,16 @@ function formatDate(prop: string) {
   const formatData = `${splitTime[0]}:${splitTime[1]} - ${newDate}`;
 
   return formatData;
+}
+
+function testIcon(props?: string): JSX.Element {
+  const fontSize = "small";
+  if (props === "v") {
+    return <Check fontSize={fontSize} sx={{ backgroundColor: "#72FF2C" }} />;
+  } else if (props === "d") {
+    return <Close fontSize={fontSize} sx={{ backgroundColor: "#FF5531" }} />;
+  } else if (props === "e") {
+    return <Remove fontSize={fontSize} sx={{ backgroundColor: "#D6DBDF" }} />;
+  }
+  return <Block fontSize={fontSize} />;
 }
--- src/pages/Escalacao/index.tsx
+++ src/pages/Escalacao/index.tsx
@@ -1,7 +1,9 @@
 import {
+  Avatar,
   Box,
   FormControl,
   FormHelperText,
+  IconButton,
   MenuItem,
   Select,
   SelectChangeEvent,
@@ -9,24 +11,45 @@ import {
 import { dataStore } from "../../store/dataStore";
 import { EsquemaEntity } from "../../entities/esquema.entity";
 import { useEffect, useState } from "react";
+import Campo from "../../img/campo.png";
+import { AtletaEntity } from "../../entities/atleta.entity";
 
 export default function Escalacao() {
-  const { esquemas } = dataStore((data) => data);
+  const { esquemas, atletasMercado } = dataStore((data) => data);
   const [esquemaSelecionado, setEsquemaSelecionado] = useState<string>("3-4-3");
   const [esquemaObject, setEsquemaObject] = useState<EsquemaEntity>(
     esquemas?.[0] as EsquemaEntity,
   );
+  const [ata, setAta] = useState<AtletaEntity[]>();
+  const [gol, setGol] = useState<AtletaEntity[]>();
+  const [lat, setLat] = useState<AtletaEntity[]>();
+  const [mei, setMei] = useState<AtletaEntity[]>();
+  const [tec, setTec] = useState<AtletaEntity[]>();
+  const [zag, setZag] = useState<AtletaEntity[]>();
 
   useEffect(() => {
     if (!esquemas || esquemas.length === 0) return;
 
-    setEsquemaSelecionado(esquemas[0].nome as string);
-    setEsquemaObject(esquemas[0]);
+    setEsquemaSelecionado(esquemas?.[0].nome as string);
+    setEsquemaObject(esquemas?.[0] as EsquemaEntity);
   }, [esquemas]);
 
   useEffect(() => {
-    console.log(esquemaObject, esquemaSelecionado);
-  }, [esquemaSelecionado]);
+    // console.log(esquemaObject, esquemaSelecionado);
+    setAta(MudarEsquema(esquemaObject?.posicoes?.ata ?? 3, ata));
+    setGol(MudarEsquema(esquemaObject?.posicoes?.gol ?? 1, gol));
+    setLat(MudarEsquema(esquemaObject?.posicoes?.lat ?? 0, lat));
+    setMei(MudarEsquema(esquemaObject?.posicoes?.mei ?? 4, mei));
+    setZag(MudarEsquema(esquemaObject?.posicoes?.zag ?? 3, zag));
+    if (atletasMercado && atletasMercado.atletas && atletasMercado.atletas[0]) {
+      const newTec: AtletaEntity[] = [];
+      newTec.push(atletasMercado.atletas[16]);
+      console.log(newTec);
+      setTec(newTec);
+    } else {
+      setTec(MudarEsquema(esquemaObject?.posicoes?.tec ?? 3, tec));
+    }
+  }, [esquemaObject]);
 
   return (
     <Box display={"flex"} gap={1}>
@@ -56,7 +79,163 @@ export default function Escalacao() {
           <FormHelperText>Esquema</FormHelperText>
         </FormControl>
       </Box>
-      <Box></Box>
+      <Box
+        sx={{
+          backgroundImage: `url(${Campo})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "60%",
+          height: 810,
+        }}
+      >
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          width={"100%"}
+          height={"100%"}
+          position={"relative"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          {/* Ataque */}
+          <Box
+            position={"absolute"}
+            width={"100%"}
+            top={120}
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+          >
+            {ata?.map((atleta, index) => AvatarJogador(atleta, index))}
+          </Box>
+          {/* Meio Campo */}
+          <Box
+            position={"absolute"}
+            display={"flex"}
+            top={280}
+            width={"100%"}
+            flexDirection={"row"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            flexWrap={"wrap"}
+          >
+            {mei?.map((atleta, index) => {
+              const width = index < 2 ? "45%" : "30%";
+              const marginBottom = index < 2 ? 10 : 0;
+              return (
+                <Box
+                  key={index}
+                  sx={{ width: width }}
+                  display={"flex"}
+                  justifyContent={"center"}
+                  marginBottom={marginBottom}
+                >
+                  {AvatarJogador(atleta, index)}
+                </Box>
+              );
+            })}
+          </Box>
+          {/* Laterais */}
+          <Box
+            position={"absolute"}
+            width={"100%"}
+            bottom={280}
+            gap={20}
+            display={"flex"}
+            flexDirection={"row"}
+            alignItems={"center"}
+            justifyContent={"space-around"}
+          >
+            {lat?.map((atleta, index) => AvatarJogador(atleta, index))}
+          </Box>
+          {/* Zagueiros */}
+          <Box
+            position={"absolute"}
+            width={"80%"}
+            bottom={180}
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+          >
+            {zag?.map((atleta, index) => AvatarJogador(atleta, index))}
+          </Box>
+          {/* Goleiro */}
+          <Box
+            position={"absolute"}
+            width={"100%"}
+            bottom={60}
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+          >
+            {gol?.map((atleta, index) => AvatarJogador(atleta, index))}
+          </Box>
+          {/* Tecnico */}
+          <Box
+            position={"absolute"}
+            width={"100%"}
+            bottom={100}
+            left={-80}
+            display={"flex"}
+            flexDirection={"row"}
+          >
+            {tec?.map((atleta, index) => AvatarJogador(atleta, index))}
+          </Box>
+        </Box>
+      </Box>
     </Box>
+  );
+}
+
+function MudarEsquema(
+  limite: number,
+  atletas?: AtletaEntity[],
+): AtletaEntity[] {
+  const novaLista: AtletaEntity[] = [];
+  const empty: AtletaEntity = {} as AtletaEntity;
+
+  if (atletas) {
+    for (let i = 0; i < atletas.length; i++) {
+      if (i < limite) {
+        novaLista.push(atletas[i]);
+      }
+    }
+  }
+
+  for (let i = 0; novaLista.length < limite; i++) {
+    novaLista.push(empty);
+  }
+
+  return novaLista;
+}
+
+function AvatarJogador(
+  atleta: AtletaEntity,
+  index: number,
+  onClick?: () => void,
+) {
+  const size = 60;
+  if (!atleta.foto)
+    return (
+      <IconButton key={index} onClick={onClick}>
+        <Avatar
+          sx={{ width: size, height: size, border: 1, borderColor: "black" }}
+        />
+      </IconButton>
+    );
+
+  const foto = atleta.foto.replace("FORMATO", "220x220");
+
+  return (
+    <IconButton key={index} onClick={onClick}>
+      <Avatar
+        src={foto}
+        alt={atleta.apelido}
+        sx={{ width: size, height: size, border: 1, borderColor: "black" }}
+      />
+    </IconButton>
   );
 }
